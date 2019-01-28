c9c56fab9a0f25ff217b9beccb27475f26762de1
Unknown <fkmontero9@gmail.com>
2019-01-28 13:05:49 +0100

    P1

---
+++ Práctica 1 - Juego del Pong/Assets/Codigo/MoverBola.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverBola : MonoBehaviour
+{
+    public float velocidadInicial = 300f;
+    public Rigidbody rig;
+    public Transform barra;
+    bool enJuego;
+    public ElementoInteractivo empezar;
+    private static float posX;
+    private static float posY;
+    private static Vector3 posicionInicial;
+    // Use this for initialization
+    void Start()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+        posicionInicial = new Vector3(posX, posY);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!enJuego)
+        {
+            if (Input.GetKeyDown("left ctrl") || empezar.pulsado)
+            {
+                enJuego = true;
+                transform.SetParent(null);
+                rig.isKinematic = false;
+                rig.AddForce(new Vector3(-velocidadInicial, velocidadInicial));
+            }
+        }
+    }
+    public void Reset()
+    {
+
+        transform.position = new Vector3(posicionInicial.x, posicionInicial.y);
+        transform.SetParent(barra);
+        enJuego = false;
+        rig.isKinematic = true;
+        rig.AddForce(Vector3.zero);
+    }
+}
---
+++ Práctica 1 - Juego del Pong/Assets/Codigo/MoverPala.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverPala : MonoBehaviour
+{
+    public float velocidad = 1f;
+    private float posY;
+    private float posX;
+    static float limiteBajo = -0.4f;
+    static float limiteAlto = 0.4f;
+    private static Vector3 posicionIni;
+    public ElementoInteractivo botonUp;
+    public ElementoInteractivo botonDown;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        posY = transform.position.y;
+        posX = transform.position.x;
+
+
+        posicionIni = new Vector3(posX, posY);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccion;
+        
+        direccion = Input.GetAxisRaw("Vertical");
+        if (botonUp.pulsado)
+        {
+            posY = posY + (1 * velocidad * Time.deltaTime);
+        }
+        else
+        {
+            if (botonDown.pulsado || Input.GetKey("up"))
+            {
+                posY = posY + (-1 * velocidad * Time.deltaTime);
+            }
+            else
+            {
+                posY = posY + (direccion * velocidad * Time.deltaTime);
+            }
+        }
+
+        if (posY >= limiteAlto)
+        {
+            posY = limiteAlto;
+            transform.position = new Vector3(posX, posY);
+        }
+        else
+        {
+            if (posY <= limiteBajo)
+            {
+                posY = limiteBajo;
+                transform.position = new Vector3(posX, posY);
+            }
+            else
+            {
+                transform.position = new Vector3(posX, posY);
+            }
+        }
+
+
+    }
+    public void Reset()
+    {
+
+        transform.position = posicionIni;
+
+        posY = transform.position.y;
+        posX = transform.position.x;
+        print(posicionIni);
+
+    }
+}
