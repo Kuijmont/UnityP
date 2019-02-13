8d182aa1bb5eb493f5350f4987d214a4da9fc07f
Unknown <fkmontero9@gmail.com>
2019-02-13 10:16:32 +0100

    golf

---
+++ MiniGolf/Assets/Code/GolfBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour
+{
+    [SerializeField]
+    private float force;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Camera.main.ScreenToWorldPoint(mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.AddForce(mousePosition * force);
+        }
+    }
+}
